ba04c02be35481c5977227c2199946b851aaa4ff
tom-obara <tom.obara@kellogg.northwestern.edu>
2020-03-14 02:52:45 +0000

    first draft

--- createdb.rb
+++ createdb.rb
@@ -15,19 +15,24 @@ end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
+events_table.insert(title: "Teriyaki Bowl", 
+                    description: "This classic joint wedged between Plaid Pantry and Domino's boasts the best teriyaki on Queen Anne, and arguably the city. People can't get enough of this family run hot spot!",
                     date: "June 21",
-                    location: "Kellogg Global Hub")
+                    location: "Lower Queen Anne")
 
 events_table.insert(title: "Kaleapolooza", 
                     description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
